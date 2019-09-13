424b42304a17e8dfe2dd46a6d84e28f9ca75913c
Israel Oliveira <israel@radek.com.br>
2019-09-12 23:46:18 -0300

    Before email to summary items

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,73 @@
+//import, setup and init firebase
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
+const db = admin.firestore()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//import and setup sendgrid
+import * as sgMail from '@sendgrid/mail';
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
+
+//send email to user after signup
+export const welcomeEmail = functions.auth.user().onCreate(
+    user => {
+        const msg = {
+            to: user.email, //format as array to send multi emails
+            from: 'amdeagencia@gmail.com',
+            templateId: TEMPLATE_ID,
+            dyamic_template_data: {
+                subject: 'Bem vindo!',
+                name: user.displayName,
+            },
+        };
+
+        return sgMail.send(msg);
+    })
+
+//send email via web for front
+export const broadcastEmail = functions.https.onCall(
+    async (data, context) => {
+        if (!context.auth && !context.auth.token.email) {
+            throw new functions.https.HttpsError('failed-precondition', 'Por favor, realize o login!')
+        };
+
+        const msg = {
+            to: context.auth.token.email,
+            from: 'amdeagencia@gmail.com',
+            templateId: TEMPLATE_ID,
+            dyamic_template_data: {
+                subject: data.subject,
+                name: data.text,
+            },
+
+        };
+        await sgMail.send(msg);
+
+        return { success: true };
+    }
+);
+
+//send email when something has a new content
+export const newOne = functions.firestore.document('posts/{postId}/comments/{comment}').onCreate(
+    async (change, context) => {
+        //read conent
+        const postSnap = await db.collection('posts').doc(context.params.postId).get();
+
+        const post = postSnap.data() || {};
+        const comment = change.data() || {};
+
+        const msg = {
+            to: post.authorEmail,
+            from: 'amdeagencia@gmail.com',
+            templateId: TEMPLATE_ID,
+            dyamic_template_data: {
+                subject: `Novo conteúdo em ${post.title}`,
+                name: post.displayName,
+                text: `${comment.user} disse ${comment.text}`
+            },
+        };
+        //send
+        return sgMail.send(msg);
+    }
+)
